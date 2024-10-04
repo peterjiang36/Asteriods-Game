82b26fb972766dc6c12b2326127db515b148e88b
Peter Jiang <peter.j@usask.ca>
2024-10-04 15:33:55 -0600

    add shooting laser to the player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
 
+signal laser_shot(laser)
+
 @export var acceleration := 10.0
 @export var max_speed := 350.0
 @export var rotation_speed := 250.0
+
+@onready var muzzle = $Muzzle
+
+var laser_scene = preload("res://Scenes/laser.tscn")
+
+var shoot_cd = false
+var rate_of_fire = 0.2
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("shoot"):
+		if !shoot_cd:
+			shoot_cd = true
+			shoot_laser()
+			await get_tree().create_timer(rate_of_fire).timeout
+			shoot_cd = false
 
 func _physics_process(delta):
 	var input_vector := Vector2(0, Input.get_axis("move_forward", "move_backward"))
@@ -29,3 +46,9 @@ func _physics_process(delta):
 		global_position.x = screen_size.x
 	elif global_position.x > screen_size.x:
 		global_position.x = 0
+		
+func shoot_laser():
+	var l = laser_scene.instantiate()
+	l.global_position = muzzle.global_position
+	l.rotation = rotation
+	emit_signal("laser_shot", l)
